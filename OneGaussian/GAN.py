33db3463aff2b3eac6391a72c460c142da5ba1b2
Edna Ruiz-Velasco <edna.ruve@gmail.com>
2018-05-14 14:52:56 +0200

    Update GAN.py

--- OneGaussian/GAN.py
+++ OneGaussian/GAN.py
@@ -1,1 +1,27 @@
 
+try:
+  import torch
+  import torch.nn as nn
+  import torch.nn.functional as F
+  
+  import argparse
+  import numpy
+  
+except ImportError,e:
+  
+  print e
+  raise SystemExit
+  
+if __name__ == '__main__':
+  
+  parser = argparse.ArgumentParser("Train a GAN for generating 1D gaussian")
+  parser.AddArgument( "-s" , "--sigma", help = "Sigma of the Gaussian", dest = "sigma", required = True, type = float)
+  parser.AddArgument( "-m" , "--mean" , help = "Mean of the Gaussian", dest = "mean", required = True, type = float)
+  
+  try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        sys.exit(0)
+        
+  
